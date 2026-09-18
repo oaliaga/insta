49d0ac7b443c67d4b76d7127d84cd8c4f2585b62
Orlando Aliaga <oaliaga@gmail.com>
2026-09-18 08:12:54 -0300

    Update UserResponse

--- app/src/main/java/com/oso/myapplication/data/response/UserResponse.kt
+++ app/src/main/java/com/oso/myapplication/data/response/UserResponse.kt
@@ -2,21 +2,22 @@ package com.oso.myapplication.data.response
 
 import com.oso.myapplication.domain.entity.UserEntity
 import com.oso.myapplication.domain.entity.UserMode
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import javax.annotation.meta.TypeQualifierNickname
 @Serializable
 data class UserResponse(
     val userId:String,
     val name:String,
-    val nickname: String,
-    val followers:Int,
-    val following:List<String>,
+    @SerialName("nickName") val nickname: String,
+    val followers:Int=0,
+    val following:List<String> = emptyList(),
     val userType:Int
 )
 
 fun UserResponse.toDomain(): UserEntity{
     val userMode = when(userType){
-        UserMode.REGULAR_USER.userType -> UserMode.REGULAR_USER
+        UserMode.REGULAR_USER.usergiType -> UserMode.REGULAR_USER
         UserMode.CONTENT_CREATOR_USER.userType -> UserMode.CONTENT_CREATOR_USER
         UserMode.COMPANY_USER.userType -> UserMode.COMPANY_USER
         else ->UserMode.REGULAR_USER
